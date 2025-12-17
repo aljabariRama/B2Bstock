a11c89d147cceb33f5be4ec53f40cadb48f12422
aljabariRama <eng.rama.jabari@gmail.com>
2025-12-17 12:11:31 +0300

    Initial commit

---
+++ test/b2b_stock.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as B2BStock from '../lib/b2b_stock-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/b2b_stock-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new B2BStock.B2BStockStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
